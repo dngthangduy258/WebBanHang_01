04f9314c25de08b7d38a74a7bc5f6648aea54547
dngthanhduy258 <dngthanhduy258@gmail.com>
2024-06-04 11:13:20 +0700

    update lab2

--- WebBanHang/Migrations/20240604012515_SeedDataToTable.Designer.cs
+++ WebBanHang/Migrations/20240604012515_SeedDataToTable.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebBanHang.Models;
 
 namespace WebBanHang.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240604012515_SeedDataToTable")]
+    partial class SeedDataToTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- WebBanHang/Models/Category.cs
+++ WebBanHang/Models/Category.cs
@@ -9,9 +9,10 @@ namespace WebBanHang.Models
     public class Category
     {
         public int Id { get; set; }
-        [Required, StringLength(50)]
+        [Required(ErrorMessage ="Chưa nhập dữ liệu"), StringLength(50)]
         public String Name { get; set; }
-        [Range(1, 100)]
+        [Range(1, 100),Required(ErrorMessage ="Chưa chọn thuộc tính")]
         public int DisplayOrder { get; set; }
+
     }
 }
